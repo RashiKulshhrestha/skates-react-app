bb5cf840a873a9706d03d30aef99e0f95312dcd5
Rashi Kulshrestha <rashikul5.rk@gmail.com>
2020-09-25 10:30:16 +0530

    Updates

---
+++ precache-manifest.30bb656b7984add9aa422ca5a2900988.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ebbd2c498144f9f389639549debcac",
+    "url": "/skates-react-app/index.html"
+  },
+  {
+    "revision": "a031ea776d8aef2f22b7",
+    "url": "/skates-react-app/static/css/main.db162cb9.chunk.css"
+  },
+  {
+    "revision": "66309d498c84e5d7d22a",
+    "url": "/skates-react-app/static/js/2.255aba3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/skates-react-app/static/js/2.255aba3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a031ea776d8aef2f22b7",
+    "url": "/skates-react-app/static/js/main.4cf2a565.chunk.js"
+  },
+  {
+    "revision": "7a3d8dd88090c28ffa77",
+    "url": "/skates-react-app/static/js/runtime-main.bd532010.js"
+  },
+  {
+    "revision": "642c50f12d70d26bf6301cf6dd7df859",
+    "url": "/skates-react-app/static/media/ALWWCMP_1.642c50f1.png"
+  },
+  {
+    "revision": "05ab308ab5c4ace63dbcc3cb799c8335",
+    "url": "/skates-react-app/static/media/rs.php.05ab308a.png"
+  }
+]);
